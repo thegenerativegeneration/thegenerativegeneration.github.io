30ccbaacfa0f0937dfd6dfd12a67d09b8a2f681d
thegenerativegeneration <6620807+thegenerativegeneration@users.noreply.github.com>
2025-12-11 13:03:13 +0000

    Deploying to gh-pages from @ thegenerativegeneration/thegenerativegeneration.github.io@fb80c94832a2bee184f3abad5dc00fc0188594c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -120,7 +120,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/haslbauer your LinkedIn user name", "_blank");
+          window.open("https://www.linkedin.com/in/haslbauer", "_blank");
         },
       },{
         id: 'social-scholar',
